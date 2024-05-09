97075782565c60a39f3e78ca7c80e3d6b6d90957
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-05-09 18:24:48 +0300

    DarkMode

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <div>
-    <button>test</button>
-    <Button variant="destructive">deneme</Button>
+  <div className="">
+    <ModeToggle/>
   </div>
   );
 }
